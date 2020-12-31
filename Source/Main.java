ad72c0ec236e94a132e4475f47054a98cff71254
akon97766 <63269090+akon97766@users.noreply.github.com>
2020-12-30 18:16:20 -0700

    Create Main.java

--- Source/Main.java
+++ Source/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-
+\\driver
     public static void main(String[] args) {
+   
         TrainingImages info = new TrainingImages();
         Classifier classifier = new Classifier(info);
     }
